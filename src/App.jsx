cbf8419db2c5c2c6a17257f46205fb781197019f
shuaibu-shehu <shuaibushehukalifa@gmail.com>
2023-12-24 19:37:46 +0530

    install vite react  and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' text-red-600 text-2xl'>his urygfj</div>
+     
+    </>
+  )
+}
+
+export default App
