4ade1fbb676c59ba08b62d5dc87f423472af6f06
shuaibu-shehu <shuaibushehukalifa@gmail.com>
2024-01-05 01:20:33 +0530

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="max-w-2xl leading-10 text-lg m-auto p-8">
+      <img src="https://cyclingmagazine.ca/wp-content/uploads/2021/01/zwift_devices.jpg" alt="" />
+      <p>
+        Immerse yourself in the world of <span className=" font-bold text-blue-800 text-2xl">TechHome</span>, where reputable retailers
+        converge to curate an unparalleled shopping experience. Unveil the joy
+        of discovering the best products while we strive to make each moment
+        with us extraordinary.
+      </p>
+    </div>
+  );
+};
